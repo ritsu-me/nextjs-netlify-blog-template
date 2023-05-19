1c294f0fd52a2389e06e52b55f69e84a13b90b94
RitsuYuzaki  ( 由崎律 ) <me@ritsu-me.com>
2023-05-19 05:01:03 +0000

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm Ritsu Yuzaki<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@ritsu_me</span>
+          <h2>[<span className="fancy">/////</span>_____]</h2>
           <SocialList />
         </div>
       </div>
